d546710c694de65f1684cba148310809432632a0
ikunhuaji <1550309783@qq.com>
2024-05-21 14:16:12 +0800

    create Listener Lesson

---
+++ Listener/src/main/java/com/starrysky/listener/MyServletContextListener.java
@@ -0,0 +1,16 @@
+package com.starrysky.listener;
+
+import jakarta.servlet.ServletContextEvent;
+import jakarta.servlet.ServletContextListener;
+
+public class MyServletContextListener implements ServletContextListener {
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+
+    }
+
+    @Override
+    public void contextDestroyed(ServletContextEvent sce) {
+
+    }
+}
